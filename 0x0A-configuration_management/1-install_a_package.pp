d617f4758b9a0fb232978ff272d32000cb709bb2
Sihle <sihle.mtshali8@gmail.com>
2024-04-19 14:22:27 -0700

    Add Puppet manifests and README

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Using Puppet, install flask from pip3
+# Using Puppet, install Flask from pip3
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+# Ensure that pip3 is installed before installing Flask
+package { 'python3-pip':
+  ensure => 'present',
+  before => Package['Flask'],
 }
