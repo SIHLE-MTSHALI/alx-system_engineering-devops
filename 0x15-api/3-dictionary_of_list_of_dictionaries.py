6cd490e22a3034d08e3684a5135de0b9d032df0c
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-05-20 12:52:22 -0700

    3. Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -15,4 +15,13 @@ if __name__ == "__main__":
 
     data = {}
     for user in users:
-        user_id =
+        user_id = user.get('id')
+        username = user.get('username')
+        user_todos = [todo for todo in todos if todo.get('userId') == user_id]
+        tasks = [{"task": todo.get('title'), "completed": todo.get('completed'),
+                  "username": username} for todo in user_todos]
+        data[user_id] = tasks
+
+    filename = "todo_all_employees.json"
+    with open(filename, mode='w') as file:
+        json.dump(data, file)
