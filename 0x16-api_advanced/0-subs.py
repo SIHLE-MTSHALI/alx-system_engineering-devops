d6e107ae6931cfeb46b59bbf915a0e7576ae6dee
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-06-04 12:22:09 -0700

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""
-This module contains the function number_of_subscribers.
-"""
-
+"""Fetches the number of subscribers for a given subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Python/requests'}
+    """Queries the Reddit API and returns the number of subscribers for a subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/your_username)"
+    }
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code == 200:
+        return response.json().get("data", {}).get("subscribers", 0)
+    else:
         return 0
-    data = response.json().get('data')
-    if data:
-        return data.get('subscribers', 0)
-    return 0
