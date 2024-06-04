4e4a3e58685f7920ad94cd2c821be95eb9c4de6b
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-06-04 12:40:05 -0700

    Add script to print top 10 hot posts for a subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 """
-This module contains the function top_ten.
+Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
 """
-
 import requests
-
 
 def top_ten(subreddit):
     """
     Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
     """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'Python/requests'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-        return
-    data = response.json().get('data')
-    if data:
-        children = data.get('children')
-        for child in children:
-            print(child.get('data').get('title'))
-    else:
-        print(None)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if data:
+            posts = data.get('children', [])
+            for post in posts:
+                print(post.get('data', {}).get('title'))
