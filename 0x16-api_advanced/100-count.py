b08b044c903efbdc2ce38ad636f3c3688efd0ac3
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-06-04 13:09:53 -0700

    3. Count it!

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -8,12 +8,15 @@ import requests
 
 def count_words(subreddit, word_list, after=None, word_count={}):
     """
-    Recursively queries the Reddit API, parses the titles of all hot articles, and prints a sorted count of given keywords.
+    Recursively queries the Reddit API, parses the titles of all hot articles,
+    and prints a sorted count of given keywords.
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Python/requests'}
     params = {'after': after, 'limit': 100}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+        url, headers=headers, params=params, allow_redirects=False
+    )
     if response.status_code != 200:
         return
 
@@ -29,12 +32,14 @@ def count_words(subreddit, word_list, after=None, word_count={}):
     for child in children:
         title = child.get('data').get('title').lower().split()
         for word in word_list:
-            word_count[word.lower()] = word_count.get(word.lower(), 0) + title.count(word.lower())
+            word_l = word.lower()
+            count = word_count.get(word_l, 0)
+            word_count[word_l] = count + title.count(word_l)
 
     if after is not None:
         return count_words(subreddit, word_list, after, word_count)
     else:
-        sorted_word_count = sorted(word_count.items(), key=lambda kv: (-kv[1], kv[0]))
-        for word, count in sorted_word_count:
+        sorted_wc = sorted(word_count.items(), key=lambda kv: (-kv[1], kv[0]))
+        for word, count in sorted_wc:
             if count > 0:
                 print("{}: {}".format(word, count))
