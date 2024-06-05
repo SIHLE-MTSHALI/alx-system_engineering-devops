ca29cbb19ba3eb93c0b736fc33d14b309aba7240
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-06-05 11:35:11 -0700

    0. Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,5 @@
-
-### Step 3: Writing the Puppet Manifest
-
-#### Puppet Manifest: `0-strace_is_your_friend.pp`
-
-```pp
-# Puppet manifest to fix the 500 Internal Server Error in Apache caused by incorrect permissions on a directory
-
+# Fix Apache 500 error by correcting wp-settings.php file permissions
 exec { 'fix-wordpress':
-  command => 'chown -R www-data:www-data /var/www/html',
+  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
