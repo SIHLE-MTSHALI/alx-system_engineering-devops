8dcc3f8f2ee2ea7780889d372314fed8e91b0cef
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-06-13 11:22:39 -0700

    0. Sky is the limit, lets bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,11 @@
-# This Puppet manifest is to configure Nginx to handle more requests by tuning its settings
-
+# Increase the ULIMIT of the default file descriptor
 exec { 'fix--for-nginx':
-  command => '/bin/sed -i "s/worker_connections 768;/worker_connections 4096;/" /etc/nginx/nginx.conf && service nginx restart',
-  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+exec { 'restart-nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
